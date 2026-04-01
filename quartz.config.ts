a4e760f097907507f97f203b9bce877b5a2cfa28
Spank <73094417+Spankadin@users.noreply.github.com>
2026-04-01 02:41:03 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "spankadin.github.io/SarosQuartz/index.html",
+    baseUrl: "spankadin.github.io/SarosQuartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
